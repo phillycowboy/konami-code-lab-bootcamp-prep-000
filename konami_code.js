2e9447b3c4b592f307a2918f81b1f3fb61f11713
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-18 20:12:12 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,64 +11,10 @@ const codes = [
   "a"
 ];
 let index=0;
-const key=e.key
+
 function init() {
-  document.body.addEventListener('keydown', function(event){
-  
-  if(key===codes[index]){
-    index++;
-    if(index===codes.length){
-      alert("Hurray!");
-      index=0;
-    }
-  }else{
-    index=0;
-  }
-  
-// let index=0
-// document.body.addEventListener('keydown', function(event){
-//   const key=e.key
-    
+  const key=event.key;
+  documnet.body.addEventListener('keydown',function(event)
 
-//   if (key === alphabet[index]) {
-//     index++;
-
-//     if (index === alphabet.length) {
-//       alert("Hurray!");
-
-//       index = 0;
-//     }
-//   } else {
-//     index = 0;
-//   }
-  
- 
-//   // if(e.key===codes.length){
-//   //   alert("Hurray!")
-    
-//   // // }else{
-//   // //   return e.preventDefault(alert)
-//   // }
-// });
-  
-
-// }
-// let index = 0;
-
-// // This is the function that would be invoked by the event listener.
-// function onKeyDownHandler(e) {
-//   const key = e.key;
-
-//   if (key === alphabet[index]) {
-//     index++;
-
-//     if (index === alphabet.length) {
-//       alert("Hurray!");
-
-//       index = 0;
-//     }
-//   } else {
-//     index = 0;
-//   }
-});
+}
   
